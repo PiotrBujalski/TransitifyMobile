0db98c02d0160201cb83747cb979ce85f7388f6b
Piotr Bujalski <p10trbuj4lsk1@gmail.com>
2023-12-15 09:06:00 +0100

    Add Ticket Menu Frontend and Backend

--- app/src/main/java/com/example/transitifymobile/TicketsMenuActivity.kt
+++ app/src/main/java/com/example/transitifymobile/TicketsMenuActivity.kt
@@ -9,8 +9,8 @@ import org.bson.Document
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.PopupMenu
@@ -18,6 +18,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.Calendar
+import java.util.Date
 
 class TicketsMenuActivity : AppCompatActivity() {
 
@@ -30,6 +32,8 @@ class TicketsMenuActivity : AppCompatActivity() {
     private lateinit var mongoClient: MongoClient
     private lateinit var mongoDatabase: MongoDatabase
     private lateinit var usersCollection: MongoCollection<Document>
+    private lateinit var ticketsCollection: MongoCollection<Document>
+    private lateinit var ticketsContainer: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tickets_menu)
@@ -39,17 +43,21 @@ class TicketsMenuActivity : AppCompatActivity() {
         ticketsImageButton = findViewById(R.id.ticketsImageButton)
         shopImageButton = findViewById(R.id.shopImageButton)
         walletImageButton = findViewById(R.id.walletImageButton)
+        ticketsContainer = findViewById(R.id.ticketsContainer)
 
         try {
             mongoClient = MongoDBHelper.connect()
             mongoDatabase = MongoDBHelper.getDatabase(mongoClient)
             usersCollection = mongoDatabase.getCollection("Users")
+            ticketsCollection = mongoDatabase.getCollection("Tickets")
         } catch (e: Exception) {
             e.printStackTrace()
             showToast("Error connecting to the database: ${e.message}")
         }
 
         fetchUserBalance()
+
+        fetchAndDisplayTickets()
 
         profileImageButton.setOnClickListener {
             showPopupMenu(it)
@@ -77,6 +85,93 @@ class TicketsMenuActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun fetchAndDisplayTickets() {
+        val userId = intent.getIntExtra("userId", -1)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val tickets = ticketsCollection.find(Document("UserId", userId)).toList()
+
+            withContext(Dispatchers.Main) {
+                ticketsContainer.removeAllViews()  // Clear existing views
+
+                for (ticket in tickets) {
+                    val ticketView = layoutInflater.inflate(R.layout.ticket_item, null)
+
+                    val typeTextView: TextView = ticketView.findViewById(R.id.ticketTypeTextView)
+                    val timeTextView: TextView = ticketView.findViewById(R.id.ticketTimeTextView)
+                    val priceTextView: TextView = ticketView.findViewById(R.id.ticketPriceTextView)
+
+                    val ticketType = ticket.getString("Type")
+                    typeTextView.text = "$ticketType"
+                    timeTextView.text = "${ticket.getInteger("Time")} minut"
+                    priceTextView.text = "${ticket.getDouble("Price")} zł"
+
+                    if (ticketType == "Ulgowy") {
+                        ticketView.setBackgroundColor(resources.getColor(android.R.color.holo_orange_light))
+                    } else {
+                        ticketView.setBackgroundColor(resources.getColor(android.R.color.holo_green_light))
+                    }
+
+                    val activateButton: Button = ticketView.findViewById(R.id.myActivateButton)
+                    val showButton: Button = ticketView.findViewById(R.id.myShowButton)
+
+                    val isActive = ticket.getBoolean("IsActive") ?: false
+                    val ticketId = ticket.getInteger("TicketId")
+                    activateButton.tag = ticketId
+
+                    val ticketTime = ticket.getInteger("Time")
+
+                    if (isActive) {
+                        activateButton.visibility = View.GONE
+                        showButton.visibility = View.VISIBLE
+                        showButton.setOnClickListener {
+                            showToast("Show button clicked for TicketId: $ticketId")
+                        }
+                    } else {
+                        activateButton.visibility = View.VISIBLE
+                        showButton.visibility = View.GONE
+                        activateButton.setOnClickListener {
+                            val clickedTicketId = it.tag as? Int
+                            if (clickedTicketId != null) {
+                                updateTicketActivationTime(clickedTicketId, ticketTime)
+                                showToast("Ticket activated successfully!")
+                            }
+                        }
+                    }
+
+                    ticketsContainer.addView(ticketView)
+                }
+            }
+        }
+    }
+
+    private fun updateTicketActivationTime(ticketId: Int, ticketTime: Int) {
+        GlobalScope.launch(Dispatchers.IO) {
+            val currentDate = Date()
+            val expirationDate = calculateExpirationDate(currentDate, ticketTime)
+
+            ticketsCollection.updateOne(
+                Document("TicketId", ticketId),
+                Document("\$set",
+                    Document("TimeActivation", currentDate)
+                        .append("TimeExpiration", expirationDate)
+                        .append("IsActive", true)
+                )
+            )
+
+            withContext(Dispatchers.Main) {
+                fetchAndDisplayTickets()
+            }
+        }
+    }
+
+    private fun calculateExpirationDate(startDate: Date, ticketTime: Int): Date {
+        val calendar = Calendar.getInstance()
+        calendar.time = startDate
+        calendar.add(Calendar.MINUTE, ticketTime)
+        return calendar.time
     }
 
     private fun fetchUserBalance() {
